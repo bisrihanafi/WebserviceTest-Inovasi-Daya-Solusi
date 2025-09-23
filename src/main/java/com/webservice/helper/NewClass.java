08dfd5d8b6696269c5092e89be587b0e7d642195
bisrihanafi <bisrihanafi@gmail.com>
2025-09-23 14:56:55 +0700

    #POINT @3

---
+++ src/main/java/com/webservice/helper/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.webservice.helper;
+
+/**
+ * Bisri Hanafi bisrihanafi@designjaya.com
+ *
+ * Sep 23, 2025 2:56:50 PM
+ * @author HP
+ */
+public class NewClass {
+
+}
